c8c547f49dc9ab8293a0fa3ac3dd133ef1eefe92
sbo <sbo@intractiv.com>
2025-06-20 07:37:47 +0200

    refactor with page objects

--- tests/mini-panier.spec.ts
+++ tests/mini-panier.spec.ts
@@ -1,30 +1,25 @@
 import { test, expect } from "@playwright/test";
+import { PageCatalogue } from "../support/page-objects/catalogue.page";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(
-    "https://simplecommerce1nz5qlcr-container-kind-robinson.functions.fnc.fr-par.scw.cloud/"
-  );
+  await page.goto("/");
 });
 
 test("À l'arrivée sur la page, le libellé du mini panier ne contient pas de chiffres", async ({
   page,
 }) => {
-  await expect(
-    page.locator("#top_page").getByRole("button", { name: /Panier/i }) // /i -> case insensitive
-  ).toHaveText("Panier");
+  const pageCatalogue = new PageCatalogue(page);
 
-  await expect(
-    page.locator("#top_page").getByRole("button", { name: /Panier/i }) // /i -> case insensitive
-  ).not.toHaveText(/\d/); // no digits
+  await expect(pageCatalogue.boutonPanier).toHaveText("Panier");
+  await expect(pageCatalogue.boutonPanier).not.toHaveText(/\d/); // no digits
 });
 
 test('À l\'arrivée sur la page, le détail est "Votre panier est vide"', async ({
   page,
 }) => {
-  await page
-    .locator("#top_page")
-    .getByRole("button", { name: /Panier/i }) // /i -> case insensitive
-    .click();
+  const pageCatalogue = new PageCatalogue(page);
+
+  await pageCatalogue.afficheMiniPanier();
 
   await expect(page.getByText("Votre panier est vide.")).toBeVisible();
 });
@@ -32,9 +27,9 @@ test('À l\'arrivée sur la page, le détail est "Votre panier est vide"', async
 test('J\'ajoute un produit, le libellé du mini panier contient "(1)"', async ({
   page,
 }) => {
-  await page.getByTestId("product-pod-add-button-209").click();
+  const pageCatalogue = new PageCatalogue(page);
 
-  await expect(
-    page.locator("#top_page").getByRole("button", { name: /Panier/i }) // /i -> case insensitive
-  ).toContainText("(1)");
+  await pageCatalogue.ajouterProduit(209);
+
+  await expect(pageCatalogue.boutonPanier).toContainText("(1)");
 });
